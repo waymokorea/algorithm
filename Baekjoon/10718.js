a5ff7ca378a99b78407ef44355f35b9506b56e02
waymokorea <taw0207@naver.com>
2022-07-13 22:24:10 +0900

    first commit

---
+++ Baekjoon/10718.js
@@ -0,0 +1,7 @@
+let str = "강한친구 대한육군"
+
+console.log(str)
+console.log(str)
+
+// 10718번 We love kriii
+// 출력 : 두줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한번씩 출력하기
---
+++ Baekjoon/2557.js
@@ -0,0 +1,5 @@
+console.log("Hello World!")
+
+
+// <2557번 문제>
+// "Hello World!" 출력하기.
