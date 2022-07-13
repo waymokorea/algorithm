50b69b969c3b37de7d03971b4a356b459f9a938c
waymokorea <taw0207@naver.com>
2022-07-13 22:41:21 +0900

    pg #1

---
+++ Programmers/짝수와 홀수.js
@@ -0,0 +1,14 @@
+function solution(num) {
+    if (num % 2 === 1 || num % 2 === -1) {
+        return 'Odd'
+    } else if (num % 2 === 0) {
+        return 'Even'
+    }
+}
+
+// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수. 
+// solution을 완성하기
+
+// 제한조건
+// num은 int범위의 정수
+// 0은 짝수이다.
