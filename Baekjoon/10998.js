49ee85e269ea75b3fe1c5d9edd732c64f2b46ae7
waymokorea <taw0207@naver.com>
2022-07-16 22:49:14 +0900

    #10998

---
+++ Baekjoon/10998.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split(' ');
+
+console.log(Number(input[0]) * Number(input[1]));
+
+
+
+/*
+10998번 문제 A x B
+
+문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.
+
+
+
+*/
