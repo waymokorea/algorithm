53c909c7169bfb19ff64177853a953ed4ecf3cb3
waymokorea <taw0207@naver.com>
2022-07-20 22:06:18 +0900

    사칙연산

---
+++ Baekjoon/10869.js
@@ -0,0 +1,34 @@
+let input = require('fs').readFileSync('dev/stdin').toString().split(' ');
+
+const num1 = Number(input[0]);
+const num2 = Number(input[1]);
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(Math.floor(num1 / num2));
+console.log(num1 % num2);
+
+
+/*
+문제
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+출력
+첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+예제 입력 1
+7 3
+
+예제 출력 1
+10
+4
+21
+2
+1
+
+첫번째 시도에는 나눗셈 부분에 floor 메서드를 적지않아서 실패했다
+*/
