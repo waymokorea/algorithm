7cf31bf37310b4955a0d81ec416717cdc1cd7e5f
waymokorea <taw0207@naver.com>
2022-10-23 17:31:22 +0900

    lv 0 개미군단

---
+++ Programmers/개미군단.js
@@ -0,0 +1,8 @@
+// 개미군단
+
+function solution(hp) {
+  let general = Math.floor(hp / 5);
+  let soldier = Math.floor((hp - general * 5) / 3);
+  let worker = hp - general * 5 - soldier * 3;
+  return general + soldier + worker;
+}
